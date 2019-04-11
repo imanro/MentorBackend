586b6cb6492ac87dc7e3a45b3895e3746612f8f7
imanro <roman.denisov@gmail.com>
2019-04-12 01:47:54 +0300

    Error fixed caused unable to run war

--- mentor-api/src/main/java/mentor/ApiApplication.java
+++ mentor-api/src/main/java/mentor/ApiApplication.java
@@ -2,10 +2,12 @@ package mentor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+@ComponentScan
+public class ApiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         System.out.println("Test");
         SpringApplication.run(ApiApplication.class, args);
